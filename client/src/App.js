0dac0925add2c49669869cd588897926d8b72a51
jaypostle <50502972+jaypostle@users.noreply.github.com>
2022-11-30 18:48:44 -0800

    test out spoonacular

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from "axios";
+import { useState, useEffect } from "react";
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.spoonacular.com/recipes/716429/information?apiKey=92319c9df23f46b19c428982982f8055"
+      );
+      console.log(response);
+      console.log(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header"></header>
     </div>
   );
 }
