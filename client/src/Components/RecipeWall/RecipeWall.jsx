13dbfd16398a6c7c34f92239917339b16292c860
jaypostle <50502972+jaypostle@users.noreply.github.com>
2022-12-06 11:07:25 -0800

    form discover code

---
+++ client/src/Components/RecipeWall/RecipeWall.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RecipeWall({ recipes }) {
+  return <div>RecipeWall</div>;
+}
+
+export default RecipeWall;
--- client/src/Pages/DiscoverRecipes/DiscoverRecipes.jsx
+++ client/src/Pages/DiscoverRecipes/DiscoverRecipes.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import axios from "axios";
 import { useState, useEffect, useRef } from "react";
+import RecipeWall from "../../Components/RecipeWall/RecipeWall";
 
 function Discover() {
   const [recipeId, setRecipeId] = useState("716429");
-  const [searchString, setSearchString] = useState(
-    "&includeIngredients=shrimp"
-  );
+  const [searchString, setSearchString] = useState("");
   const [singleRecipeData, setSingleRecipeData] = useState();
   const [recipesSearchResults, setRecipesSearchResults] = useState([]);
 
   useEffect(() => {
     // fetchSingleRecipeData(recipeId);
     // fetchRecipesSearchData(searchString);
-  }, []);
+  }, [searchString]);
 
   const apiKey = "92319c9df23f46b19c428982982f8055";
   const baseURL = "https://api.spoonacular.com/recipes";
@@ -49,6 +48,7 @@ function Discover() {
   const [discoverParams, setDiscoverParams] = useState({
     mealType: "dinner",
     search: null,
+    fridgeSearch: null,
     discoverFridge: [],
     dietaryPreferences: [],
     allergens: [],
@@ -56,7 +56,8 @@ function Discover() {
 
   useEffect(() => {
     console.log(discoverParams);
-  }, [discoverParams]);
+    console.log(searchString);
+  }, [discoverParams, searchString]);
 
   //**
   // Form References
@@ -70,22 +71,76 @@ function Discover() {
   };
 
   const handleCheckBoxChange = (e) => {
-    console.log(e.target.name, ":", e.target.value);
+    // console.log(e.target.name, ":", e.target.value);
 
     // find that key
-    console.log(e.target.checked);
+    // console.log(e.target.checked);
     // if (discoverParams[e.target.name].length === 0) {
     if (e.target.checked) {
       // this will add the checkbox target value into the array of that name
+      const newArray = [...discoverParams[e.target.name], e.target.value];
       setDiscoverParams({
         ...discoverParams,
-        ...[e.target.name],
-        [e.target.name]: e.target.value,
+        [e.target.name]: newArray,
+      });
+    } else {
+      // find that item and take it out
+      const filteredArray = discoverParams[e.target.name].filter(
+        (item) => item !== e.target.value
+      );
+      setDiscoverParams({
+        ...discoverParams,
+        [e.target.name]: filteredArray,
       });
     }
+  };
 
-    // push that value into the array (slice)
-    // or find it and slice it out
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    const searchParamsArray = [];
+    // take the discoverParams object
+
+    // mealType = type
+    let type;
+    if (discoverParams.mealType) {
+      type = `type=${discoverParams.mealType}`;
+      searchParamsArray.push(type);
+    }
+    // search = query
+    let query;
+    if (discoverParams.search) {
+      query = `query=${discoverParams.search}`;
+      searchParamsArray.push(query);
+    }
+
+    // allergens = intolerances
+    let intolerances; 
+    // TURN THIS INTO ONE FUNCTION?
+    if (discoverParams.allergens.length > 0) {
+      if (discoverParams.allergens.length === 1) {
+        intolerances = `intolerances=${discoverParams.allergens[0]}`;
+      } else {
+        intolerances = `intolerances=${discoverParams.allergens.join("|")}`;
+      }
+      searchParamsArray.push(intolerances);
+    }
+    // dietaryPreferences = diet
+    // discoverFridge = includeIngredients
+    // fridgeSearch // push this to the discover fridge array
+
+    // turn that into a searchString
+    console.log(searchParamsArray);
+
+    const newSearchString = searchParamsArray
+      .map((param) => {
+        return `&${param}`;
+      })
+      .join("");
+
+    console.log(newSearchString);
+
+    setSearchString(newSearchString);
   };
 
   //  how to design custom checkboxes https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_custom_checkbox
@@ -95,7 +150,11 @@ function Discover() {
       <h1>Discover Recipes</h1>
 
       {/* {recipe && recipe.aggregateLikes} */}
-      <form className="discover-recipes-form" ref={formRef}>
+      <form
+        className="discover-recipes-form"
+        ref={formRef}
+        onSubmit={handleFormSubmit}
+      >
         {/*  Select Field */}
         <label htmlFor="meal-type" className="discover__label">
           Meal Type
@@ -115,11 +174,19 @@ function Discover() {
             >
               Breakfast
             </option>
-            <option type="text" className="discover__meal-type" value="lunch">
-              Lunch
+            <option
+              type="text"
+              className="discover__meal-type"
+              value="appetizer"
+            >
+              Appetizer
             </option>
-            <option type="text" className="discover__meal-type" value="dinner">
-              Dinner
+            <option
+              type="text"
+              className="discover__meal-type"
+              value="main course"
+            >
+              Main Course
             </option>
             <option type="text" className="discover__meal-type" value="dessert">
               Dessert
@@ -137,15 +204,15 @@ function Discover() {
             }}
           />
         </label>
-        <div className="discover__fridge-wrapper">
+        <div className="discover-fridge-wrapper">
           What's in your fridge
-          <div className="discover__fridge-wrapper--checkboxes">
+          <div className="discover-fridge-wrapper--checkboxes">
             <label className="discover__label--checkbox">
               Chicken
               <input
                 name="discoverFridge"
                 type="checkbox"
-                className="discover__fridge"
+                className="discover-fridge"
                 value="chicken"
                 onClick={handleCheckBoxChange}
               />
@@ -155,7 +222,7 @@ function Discover() {
               <input
                 name="discoverFridge"
                 type="checkbox"
-                className="discover__fridge"
+                className="discover-fridge"
                 value="beef"
                 onClick={handleCheckBoxChange}
               />
@@ -165,18 +232,18 @@ function Discover() {
               <input
                 name="discoverFridge"
                 type="checkbox"
-                className="discover__fridge"
+                className="discover-fridge"
                 value="onions"
                 onClick={handleCheckBoxChange}
               />
             </label>
           </div>
           <label className="discover__label">
-            What's in your fridge - Search
+            Search
             <input
               name="fridgeSearch"
               type="search"
-              className="discover__fridge"
+              className="allergens"
               onChange={(e) => {
                 handleChange(e);
               }}
@@ -185,15 +252,80 @@ function Discover() {
         </div>
         <label className="discover__label">
           Dietary Preferences
-          <input type="text" className="discover__dietary-preferences" />
+          <div className="discover__dietary-preferences-wrapper--checkboxes">
+            <label className="discover__label--checkbox">
+              Ketogenic
+              <input
+                name="dietaryPreferences"
+                type="checkbox"
+                className="dietary-preferences"
+                value="ketogenic"
+                onClick={handleCheckBoxChange}
+              />
+            </label>
+            <label className="discover__label--checkbox">
+              Vegetarian
+              <input
+                name="dietaryPreferences"
+                type="checkbox"
+                className="dietary-preferences"
+                value="vegetarian"
+                onClick={handleCheckBoxChange}
+              />
+            </label>
+            <label className="discover__label--checkbox">
+              Gluten-Free
+              <input
+                name="dietaryPreferences"
+                type="checkbox"
+                className="allergens"
+                value="gluten-free"
+                onClick={handleCheckBoxChange}
+              />
+            </label>
+          </div>
         </label>
         <label className="discover__label">
           Allergens
-          <input type="text" className="discover__allergens" />
+          <div className="discover__allergens-wrapper--checkboxes">
+            <label className="discover__label--checkbox">
+              Tree Nuts
+              <input
+                name="allergens"
+                type="checkbox"
+                className="allergens"
+                value="tree nut"
+                onClick={handleCheckBoxChange}
+              />
+            </label>
+            <label className="discover__label--checkbox">
+              Shellfish
+              <input
+                name="allergens"
+                type="checkbox"
+                className="allergens"
+                value="shellfish"
+                onClick={handleCheckBoxChange}
+              />
+            </label>
+            <label className="discover__label--checkbox">
+              Dairy
+              <input
+                name="allergens"
+                type="checkbox"
+                className="allergens"
+                value="dairy"
+                onClick={handleCheckBoxChange}
+              />
+            </label>
+          </div>
         </label>
         {/* Place Sorts here */}
         {/* Sort by Iron, Salt, Sugar etc */}
+        <button type="submit">Discover Recipes</button>
       </form>
+
+      <RecipeWall recipes="placeholder" />
     </div>
   );
 }
